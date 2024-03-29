0fa6cdec39eb6b856a3294bd99fe418d77e75060
Alex <alexcampbell1124@gmail.com>
2024-03-29 14:06:34 -0500

    Added basic graphics, player and world scenes, and basic mouse movement components

---
+++ components/movement_input_component.gd
@@ -0,0 +1,13 @@
+class_name MovementInputComponent
+extends Node2D
+
+@export var movement_component : MovementComponent
+
+var click_position : Vector2
+
+func _input(event: InputEvent) -> void:
+	if Input.is_action_just_released("left_click"):
+		
+		click_position = get_global_mouse_position()
+		
+	movement_component.click_position = click_position
