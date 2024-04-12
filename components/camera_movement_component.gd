b27afcf5c0b581449efa1e5e7a54427519ce46df
Alex <alexcampbell1124@gmail.com>
2024-04-12 17:32:44 -0500

    Prototyping rts selector box mechanics

---
+++ components/camera_movement_component.gd
@@ -0,0 +1,44 @@
+class_name CameraMovementComponent
+extends MovementComponent
+
+@export var camera : Camera2D
+
+var viewport : Vector2
+var mouse_position : Vector2
+var direction : Vector2
+var x_axis_direction : float
+var y_axis_direction : float
+
+func _process(delta: float) -> void:
+	
+	viewport = get_viewport().size
+	
+	mouse_position = get_viewport().get_mouse_position()
+	
+	if (mouse_position.x >= -1 and mouse_position.x <= 5):
+		
+		x_axis_direction = -1
+		
+	elif (mouse_position.x <= viewport.x and mouse_position.x >= viewport.x - 5):
+		
+		x_axis_direction =  1
+		
+	else:
+		
+		x_axis_direction = 0
+		
+	if (mouse_position.y >= -1 and mouse_position.y  <= 5):
+		
+		y_axis_direction =  -1
+		
+	elif (mouse_position.y <= viewport.y and mouse_position.y >= viewport.y - 5):
+		
+		y_axis_direction =  1
+		
+	else:
+		
+		y_axis_direction = 0
+		
+	direction = Vector2(x_axis_direction, y_axis_direction).normalized()
+	
+	camera.translate(direction * 2400 * delta) 
--- scripts/camera.gd
+++ scripts/camera.gd
@@ -1,40 +1,93 @@
 extends Camera2D
 
-var viewport : Vector2
 var mouse_position : Vector2
-var direction : Vector2
-var x_axis_direction : float
-var y_axis_direction : float
+var mouse_global_position : Vector2
+var start : Vector2
+var start_vector : Vector2
+var end : Vector2
+var end_vector : Vector2
+var is_dragging : bool = false
+signal area_selected
+signal start_move_selection
+
+@onready var UI : CanvasLayer = $".."
+var selector_box : Panel
+
+func _ready() -> void:
+	
+	pass
 
 func _process(delta: float) -> void:
 	
-	viewport = get_viewport().size
-	mouse_position = get_global_mouse_position()
+	#print(selector_box)
 	
-	if (mouse_position.x >= -1 and mouse_position.x <= 5):
+	if Input.is_action_just_pressed("left_click"):
 		
-		x_axis_direction = -1
+		start = mouse_global_position
 		
-	elif (mouse_position.x <= viewport.x and mouse_position.x >= viewport.x - 5):
+		start_vector = mouse_position
 		
-		x_axis_direction =  1
+		is_dragging = true
 		
-	else:
-		
-		x_axis_direction = 0
-		
-	if (mouse_position.y >= -1 and mouse_position.y  <= 5):
-		
-		y_axis_direction =  -1
-		
-	elif (mouse_position.y <= viewport.y and mouse_position.y >= viewport.y - 5):
-		
-		y_axis_direction =  1
-		
-	else:
-		
-		y_axis_direction = 0
-		
-	direction = Vector2(x_axis_direction, y_axis_direction).normalized()
+		selector_box = Panel.new()
 	
-	translate(direction * 2400 * delta) 
+		UI.add_child(selector_box)
+		
+	if is_dragging:
+		
+		end = mouse_global_position
+		
+		end_vector = mouse_position
+		
+		draw_area()
+		
+		for child in selector_box.get_children():
+			print(child.name)
+		
+	if Input.is_action_just_released("left_click"):
+		
+		if start_vector.distance_to(mouse_position) > 20:
+			
+			end = mouse_global_position
+			
+			end_vector = mouse_position
+			
+			is_dragging = false
+			
+			selector_box.visible = false
+			
+			draw_area(false)
+			
+			emit_signal("area_selected")
+			
+		else:
+			
+			end = start
+			
+			is_dragging = false
+			
+			selector_box.visible = false
+			
+			draw_area(false)
+			
+func _input(event):
+	
+	if event is InputEventMouse:
+		
+		mouse_position = event.position
+		
+		mouse_global_position = get_global_mouse_position()
+		
+func draw_area(s=true):
+	
+	selector_box.size = Vector2(abs(start_vector.x - end_vector.x), abs(start_vector.y - end_vector.y))
+	
+	var pos = Vector2()
+	
+	pos.x = min(start_vector.x, end_vector.x)
+	
+	pos.y = min(start_vector.y, end_vector.y)
+	
+	selector_box.position = pos
+	
+	selector_box.size *= int(s)
